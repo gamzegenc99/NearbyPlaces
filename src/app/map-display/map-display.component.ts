7fc82425b3066b6e6152c11b2cb4560ad6edfbf9
gamzegenc99 <gamzegenc99@gmail.com>
2023-10-25 01:22:06 +0300

    Adding the application to make an HTTP GET request to a local backend server running on localhost to retrieve the data based on the entered coordinates. The backend server is responsible for providing data from a data source such as the Google Places API.

--- src/app/map-display/map-display.component.ts
+++ src/app/map-display/map-display.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 // Import the google.d.ts declaration
 import '../../google.d.ts';
@@ -6,10 +9,18 @@ import '../../google.d.ts';
 
 @Component({
   selector: 'app-map-display',
-  template: '<div id="map" style="width: 100%; height: 400px;"></div>',
+  template: '<div id="map" style="width: 100%; height: 400px;"></div><br/><div *ngFor="let place of places">{{ place }}</div>',
   styleUrls: ['./map-display.component.css']
 })
 export class MapDisplayComponent implements OnInit {
+
+  public places: string[] = [];
+  private apiUrl = 'http://localhost:8080/places'; // Adjust the URL as needed
+  constructor(private http: HttpClient) {}
+
+  getPlaces(latitude:any, longitude:any, radius:any): Observable<any> {
+    return this.http.get("http://localhost:8080/api/places/search?longitude="+longitude+"&latitude="+latitude+"&radius="+radius);
+  }
 
 
   ngOnInit(): void {
@@ -17,12 +28,33 @@ export class MapDisplayComponent implements OnInit {
     
   }
 
+  initMapwithParameter(latitude:any, longitude:any, radius:any) {
+    debugger;
+    // Create a new Google Map instance1
+    const map = new google.maps.Map(document.getElementById('map')!, {
+      center: {lat: parseFloat(latitude), lng: parseFloat(longitude)}, 
+      zoom: 12
+    });
+
+
+    // Add a marker to the map
+    const marker = new google.maps.Marker({
+      position: { lat: parseFloat(latitude), lng: parseFloat(longitude)},
+      map: map,
+      title: 'Selected Coordinates'
+    });
+    this.getPlaces(latitude,longitude,radius).subscribe((data) => {
+      this.places = data;
+    });
+  }
+  
   initMap() {
     // Create a new Google Map instance1
     const map = new google.maps.Map(document.getElementById('map')!, {
       center: { lat: 40.7128, lng: -74.0060 }, // New York City coordinates
       zoom: 12
     });
+
 
     // Add a marker to the map
     const marker = new google.maps.Marker({
@@ -31,6 +63,7 @@ export class MapDisplayComponent implements OnInit {
       title: 'New York City'
     });
   }
+
 
   loadGoogleMaps() {
     const apiKey = environment.key; // API anahtarını config dosyasından alınması
